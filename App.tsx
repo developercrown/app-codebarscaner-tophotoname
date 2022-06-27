85395eec299e943a352b8218d2b38b73d9dab2ad
Rene Corona <ingeniero.rene.corona@gmail.com>
2022-06-27 14:56:56 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Audio } from 'expo-av';
 
 export default function App() {
+  const [sound, setSound] = useState<any>();
+  const [scannedCode, setScannedCode] = useState<String | null>(null);
+  const [hasPermission, setHasPermission] = useState<any>(null);
+  const [scanned, setScanned] = useState<any>(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = (props: any) => {
+    let { type, data } = props
+    setScanned(true);
+    setScannedCode(data);
+    // playSound();
+    alert(data);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
   );
 }
@@ -18,3 +50,79 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+/*
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { useEffect, useState } from 'react';
+import { Audio } from 'expo-av';
+
+export default function App() {
+  const [sound, setSound] = useState<any>();
+  const [scannedCode, setScannedCode] = useState<String | null>(null);
+  const [hasPermission, setHasPermission] = useState<any>(null);
+  const [scanned, setScanned] = useState<any>(false);
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(
+      require('./assets/audio/short.mp3')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+
+  const handleBarCodeScanned = (props: any) => {
+    let { type, data } = props
+    setScanned(true);
+    setScannedCode(data);
+    playSound();
+    alert(data);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  // useEffect(() => {
+  //   return sound
+  //     ? () => {
+  //         console.log('Unloading Sound');
+  //         sound.unloadAsync(); }
+  //     : undefined;
+  // }, [sound]);
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+*/
